dd336b3077571f970697e3ad119c494fd9d0a6bb
vhietala <vhietala@gmail.com>
2018-01-31 15:36:34 +0200

    commit

---
+++ src/app/front/front.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpErrorResponse} from '@angular/common/http';
+import {MediaService} from '../services/media.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-front',
+  templateUrl: './front.component.html',
+  styleUrls: ['./front.component.scss']
+})
+export class FrontComponent implements OnInit {
+
+  constructor(private mediaService: MediaService, private router: Router) {
+  }
+
+  ngOnInit() {
+    if (localStorage.getItem('token') !== null) {
+      this.mediaService.getUserData().subscribe(response => {
+        console.log('Welcome ' + response['full_name']);
+      }, (error: HttpErrorResponse) => {
+        console.log(error);
+        this.router.navigate(['login']);
+      });
+    } else {
+      this.router.navigate(['login']);
+    }
+  }
+
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {MediaService} from '../services/media.service';
+import {Router} from '@angular/router';
+import {HttpErrorResponse} from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+
+  constructor(public mediaService: MediaService, private router: Router) {
+  }
+
+  ngOnInit() {
+    if (localStorage.getItem('token') !== null) {
+      this.mediaService.getUserData().subscribe(response => {
+        this.router.navigate(['front']);
+      }, (error: HttpErrorResponse) => {
+        console.log(error);
+      });
+    }
+  }
+
+}
